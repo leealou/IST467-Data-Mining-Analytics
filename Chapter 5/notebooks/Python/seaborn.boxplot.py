c8b45489a87b9749d186fb34a2a6cac332e8e73e
Alexis Lee <AlexisLee1999@gmail.com>
2022-04-11 21:19:34 -0400

    Chapter 5 upload

---
+++ Chapter 5/notebooks/Python/seaborn.boxplot.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn import datasets
+import seaborn as sns
+
+boston = datasets.load_boston()
+
+BostonDF = pd.DataFrame(boston.data, columns=boston.feature_names)
+
+Predictors = BostonDF[BostonDF.columns[0:12]]
+
+sns.set(style="ticks")
+sns.boxplot(data = Predictors)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+DataScaled = scaler.fit_transform(Predictors)
+
+
+sns.set(style="ticks")
+sns.boxplot(data = DataScaled)
